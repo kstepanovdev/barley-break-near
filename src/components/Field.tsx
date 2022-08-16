1ef791f9bc484ad4356fa58f0deb672d36de6eb5
Kyrylo Stepanov <k.stepanov@tutanota.com>
2022-08-17 02:37:43 +0300

    added: initial design for the game's field

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,38 +1,74 @@
-import React from 'react';
-import init, { init_field } from 'barkley-break'
+import React, { createElement, DOMElement, ReactElement } from 'react';
+import init, { init_field, swap, is_solved } from 'barkley-break'
 
 interface IFieldState {
     field: Uint8Array,
     solved: boolean
+    move: IFieldMove
 }
 interface IFieldProps {}
+
+interface IFieldMove {
+    from: null | number,
+    to: null | number
+}
 
 class Field extends React.Component<IFieldProps, IFieldState> {
     constructor(props: IFieldProps) {
         super(props);
         this.state = {
           field: Uint8Array.from([]),
-          solved: false 
+          solved: false,
+          move: { from: null, to: null }
         };
     }
-    
+
+    makeMove(index: number, cell_val: number) {
+        let from = this.state.move.from;
+        let to = this.state.move.to;
+
+        if (from === null) {
+            from = cell_val
+        } else if (to === null) {
+            to = cell_val
+        } else {
+            this.setState({ field: swap(this.state.field, from, to) })
+        }
+    }
+
     componentDidMount(): void {
         init().then(() => {
-            this.setState({ 
+            this.setState({
                 field: init_field(),
             })
         })
     }
-    
+
     render() {
-        let cells: Array<JSX.Element> = []; 
+        let row_el: ReactElement;
+        let cells: Array<ReactElement> = [];
+        let rows: Array<ReactElement> = [];
+
         this.state.field.forEach((cell_val: number, index: number) => {
-            cells.push(<button className="square" key={index}>{ cell_val }</button>)
-        }) 
+            if ((index !== 0 && index % 4 === 0) || index === 15) {
+                if (index === 15) {
+                    cells.push(<button className="square" key={index} onClick={ () => this.makeMove(index, cell_val)} >{ cell_val }</button>)
+                }
+
+                row_el = React.createElement(
+                    "div",
+                    { className: 'board-row', key: index / 4},
+                    [...cells]
+                )
+                cells = []
+                rows.push(row_el)
+            }
+            cells.push(<button className="square" key={index} onClick={ () => this.makeMove(index, cell_val)} >{ cell_val }</button>)
+        })
 
         return(
             <div className="game-field">
-                {cells}
+                {rows}
             </div>
         )
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,27 @@
-use wasm_bindgen::prelude::*;
-use rand::thread_rng;
 use rand::seq::SliceRandom;
+use rand::thread_rng;
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn init_field() -> js_sys::Uint8Array {
     let mut field = (0..=15).collect::<Vec<u8>>();
     field.shuffle(&mut thread_rng());
-    
+
     js_sys::Uint8Array::from(&field[..])
+}
+
+#[wasm_bindgen]
+pub fn swap(mut field: Vec<u8>, from: usize, to: usize) -> js_sys::Uint8Array {
+    field.swap(from, to);
+
+    js_sys::Uint8Array::from(&field[..])
+}
+
+#[wasm_bindgen]
+pub fn is_solved(field: Vec<u8>) -> bool {
+    if field[field.len() - 1] != 0 {
+        false
+    } else {
+        field[0..=15] == (1..15).collect::<Vec<u8>>()
+    }
 }
